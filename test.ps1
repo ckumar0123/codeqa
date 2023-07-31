96a99b3204bdd2bb1c07bad39aa3d4fbf6a53d31
ckumar0123 <132701941+ckumar0123@users.noreply.github.com>
2023-07-31 23:20:21 +0530

    Update test.ps1
    test2

--- test.ps1
+++ test.ps1
@@ -1,4 +1,6 @@
 $User = "cksingh"
-$Password = "test@123"
+$Password = "test@1230"
 az login -user $User -password $Password
 $Secure_String_Pwd = ConvertTo-SecureString "P@ssW0rD!" -AsPlainText -Force
+az login -user $User -password $Password
+
