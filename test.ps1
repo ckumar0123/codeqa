9a269c5def8779dcc873c9075c378ae52bf50da3
ckumar0123 <132701941+ckumar0123@users.noreply.github.com>
2023-08-01 17:30:49 +0530

    Update test.ps1
    Test2

--- test.ps1
+++ test.ps1
@@ -1,5 +1,5 @@
-$User = "cksingh"
-$Password = "test@123"
-az login -user $User -password $Password
+param ( $username, $Password)
 $Secure_String_Pwd = ConvertTo-SecureString "P@ssW0rD!" -AsPlainText -Force
 Set-Secret -Name FirstPassword -Secret "Password1!"
+
+
